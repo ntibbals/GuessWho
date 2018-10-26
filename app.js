3353c14c76eba153baf8ff186604df1336ff89d9
ntibbals <ntibbals@outlook.com>
2018-10-25 20:54:47 -0700

    recent alterations to design

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-"use strict";
+//  First question.
+
 console.log('Checking userName before prompt, should be invalid', userName);
 var userName = prompt('Thanks for your interest. It\'s a pleasure to meet you virtually! If you don\'t mind, what\'s your name?'); //First quesiton prompt for name
 console.log('Checking userName after prompt, should be valid', userName);
@@ -18,11 +19,11 @@ function questOne() {
   var responseOne = prompt('To start, ' + userName + ', try guessing if I am originally from the Seattle area?').toUpperCase(); //first response including if/else statements on response, forcing each answer to be stored in Upper Case
   //subsequent if then else statements quantify if 'Y,y,Yes,yes alert user accordingly, however if 'N,n,No,no' then(else) alert user accordingly
   console.log('Checking responseOne after prompt, should be valid', responseOne);
-  if (responseOne === 'Y' || responseOne === 'YES') {
-    alert('Unfortunately, no. I was actually born in Spoakne Valley, WA!');
-  } else {
+  if (responseOne === 'N' || responseOne === 'NO') {
     alert('That\'s right! I\'m not originally from the Pudget Sound. I\'m actually from the east side of the state.');
     numCorrect++;
+  } else {
+    alert('Unfortunately, no. I was actually born in Spoakne Valley, WA!');
   }
 }
 questOne();
@@ -32,11 +33,11 @@ function questTwo() {
   var responseTwo = prompt('Next, do you think I own a dog?').toUpperCase();//second response including if/else statements on response, forcing each answer to be stored in Upper Case
   console.log('Checking responseTwo after prompt, should be valid', responseTwo);
   //subsequent if then else statements quantify if 'Y,y,Yes,yes alert user accordingly, however if 'N,n,No,no' then(else) alert user accordingly
-  if (responseTwo === 'Y' || responseTwo === 'YES') {
-    alert('I hate to admit this, but no. I was actually roped into taking in two rescue cats who are actually brother and sister. All thanks to my beautiful girlfriend.');
-  } else {
+  if (responseTwo === 'N' || responseTwo === 'NO') {
     alert('You\'re right! I\'m currently living downtown without a backyard so as a result, my girlfriend and I have two cats.');
     numCorrect++;
+  } else {
+    alert('I hate to admit this, but no. I was actually roped into taking in two rescue cats who are actually brother and sister. All thanks to my beautiful girlfriend.');
   }
 }
 console.log('Checking number correct', numCorrect);
@@ -46,11 +47,11 @@ function questThree() {
   var responseThree = prompt('Once again, thanks for playing. Next, try guessing whether I am an only sibling?').toUpperCase();
   console.log('Checking responseThree after prompt, should be valid', responseThree);
   //subsequent if then else statements quantify if 'Y,y,Yes,yes alert user accordingly, however if 'N,n,No,no' then(else) alert user accordingly
-  if (responseThree === 'Y' || responseThree === 'YES') {
-    alert('No. I was blessed with an awesome brother and sister!');
-  } else {
+  if (responseThree === 'N' || responseThree === 'NO') {
     alert('You\'re right. I have both an incredible brother named Michael-Ray and awesome sister named Nicole');
     numCorrect++;
+  } else {
+    alert('No. I was blessed with an awesome brother and sister!');
     console.log('Checking number correct', numCorrect);
   }
 }
@@ -129,7 +130,7 @@ function questSeven() {
       numCorrect++;
       break;
     } else {
-      animalGuess = prompt('Sorry. Try again').toUpperCase(); z
+      animalGuess = prompt('Sorry. Try again').toUpperCase();
       counter2++;
     }
   }
